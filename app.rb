33fc7f9b776ce71a30e62c14fd175f3768171723
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-07 04:51:07 +0000

    initial commit adding /rock path

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+get("/rock") do
+  "this string will be rock"
 end
